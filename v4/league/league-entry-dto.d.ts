3adbcf0679e7cabe1e5d4a7214e27ba005dabfe7
Yuto Otaguro <yotagur@gmail.com>
2019-04-29 19:43:02 -0500

    Create interface for by-summoner

---
+++ v4/league/league-entry-dto.d.ts
@@ -0,0 +1,22 @@
+import { MiniSeriesDTO } from "./mini-series-dto";
+
+export interface LeagueEntryDTO {
+    queueType: string;
+    summonerName: string;
+    hotStreak: boolean;
+    miniSeries: MiniSeriesDTO;
+    wins: number;
+    veteran: boolean;
+    losses: number;
+    rank: string;
+    leagueId: string;
+    inactive: boolean;
+    freshBlood: boolean;
+    tier: string;
+
+    /**
+     * Player's summonerId (Encrypted) 
+     */
+    summonerId: string;
+    leaguePoints: number;
+}
