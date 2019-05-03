a4e24917b8c3adbcf7fc953c156b273ebe489891
Yuto Otaguro <yotagur@gmail.com>
2019-05-03 16:50:13 -0500

    Create data-dragon interfaces

---
+++ data-dragon/versions/dd-realms.d.ts
@@ -0,0 +1,21 @@
+export interface IDDRealms {
+    n: {
+        item: string,
+        rune: string,
+        mastery: string,
+        summoner: string,
+        champion: string,
+        profileicon: string,
+        map: string,
+        language: string,
+        sticker: string
+    },
+    v: string,
+    l: string,
+    cdn: string,
+    dd: string,
+    lg: string,
+    css: string,
+    profileiconmax: number,
+    store: any
+}
