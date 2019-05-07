004cb34908ea5021ef494ca6888ac41055fea21a
Yuto Otaguro <yotagur@gmail.com>
2019-05-06 23:11:59 -0500

    Create index file to v4

---
+++ v4/index.d.ts
@@ -0,0 +1,47 @@
+// Champion
+export * from './champion/champion-info';
+
+// Champion mastery
+export * from './champion-mastery/champion-mastery-dto'
+
+// League
+export * from './league/league-entry-dto';
+export * from './league/league-item-dto';
+export * from './league/league-list-dto'
+export * from './league/mini-series-dto'
+
+// lol-status
+export * from './lol-status/incident';
+export * from './lol-status/message';
+export * from './lol-status/services';
+export * from './lol-status/shard-status';
+export * from './lol-status/translation';
+
+// match
+export * from './match/mastery-dto';
+export * from './match/match-dto';
+export * from './match/match-list-dto';
+export * from './match/match-reference-dto';
+export * from './match/participant-dto';
+export * from './match/participant-identity-dto';
+export * from './match/participant-stats-dto';
+export * from './match/participant-timeline-dto';
+export * from './match/player-dto';
+export * from './match/rune-dto';
+export * from './match/team-bans-dto';
+export * from './match/team-stats-dto';
+
+// spectator
+export * from './spectator/banned-champion';
+export * from './spectator/featured-game-info';
+export * from './spectator/featured-games';
+export * from './spectator/observer';
+export * from './spectator/participant';
+
+// summoner
+export * from './summoner/summoner-dto';
+
+// tournament
+export * from './tournament/lobby-event-dto';
+export * from './tournament/lobby-event-dto-wrapper';
+export * from './tournament/tournament-code-dto';
