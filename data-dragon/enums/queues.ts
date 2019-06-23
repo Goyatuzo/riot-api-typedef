be3a69861cf616631232e6c0ceed876c268fac55
Yuto Otaguro <yotagur@gmail.com>
2019-06-23 15:40:50 -0500

    Complete matchmaking queue listing

---
+++ data-dragon/enums/queues.ts
@@ -0,0 +1,168 @@
+export enum HowlingAbyssQueues {
+    /**
+     * 1v1 Snowdown Showdown games
+     */
+    SNOWDOWN_SHOWDOWN_1v1 = 72,
+    /**
+     * 2v2 Snowdown Showdown games
+     */
+    SNOWDOWN_SHOWDOWN_2v2 = 73,
+    /**
+     * One For All: Mirror Mode games
+     */
+    HOWLING_ABYSS_OFA_MIRROR = 78,
+    /**
+     * 5v5 ARAM games
+     */
+    ARAM = 450,
+    /**
+     * Legend of the Poro King games
+     */
+    LEGEND_OF_THE_PORO_KING = 920
+}
+
+export enum SummonersRiftQueues {
+    /**
+     * 6v6 Hexakill games
+     */
+    HEXAKILL = 75,
+    /**
+     * Ultra Rapid Fire games
+     */
+    URF = 76,
+    /**
+     * Co-op vs AI Ultra Rapid Fire games
+     */
+    URF_COOP = 83,
+    /**
+     * Nemesis games
+     */
+    NEMESIS = 310,
+    /**
+     * Black Market Brawlers games
+     */
+    BLACK_MARKET_BRAWLERS = 313,
+    /**
+     * All Random games
+     */
+    ALL_RANDOM = 325,
+    /**
+     * 5v5 Draft Pick games
+     */
+    DRAFT_PICK = 400,
+    /**
+     * 5v5 Ranked Solo games
+     */
+    RANKED_SOLO = 420,
+    /**
+     * 5v5 Blind Pick games
+     */
+    BLINK_PICK = 430,
+    /**
+     * 5v5 Ranked Flex games
+     */
+    RANKED_FLEX = 440,
+    /**
+     * Blood Hunt Assassin games
+     */
+    BLOOD_HUNT_ASSASSIN = 600,
+    /**
+     * Clash games
+     */
+    CLASH = 700,
+    INTRO_BOTS = 830,
+    BEGINNER_BOTS = 840,
+    INTERMEDIATE_BOTS = 850,
+    /**
+     * ARURF games
+     */
+    ARURF = 900,
+    /**
+     * Nexus Siege games
+     */
+    NEXUS_SIEGE = 940,
+    /**
+     * Doom Bots Voting games
+     */
+    DOOM_BOTS_VOTING = 950,
+    /**
+     * Doom Bots Standard games
+     */
+    DOOM_BOTS_STANDARD = 960,
+    /**
+     * Snow ARURF games
+     */
+    SNOW_ARURF = 1010,
+    ONE_FOR_ALL = 1020
+}
+
+export enum TwistedTreelineQueues {
+    /**
+     * 6v6 Hexakill games
+     */
+    HEXAKILL = 98,
+    /**
+     * 3v3 Blind Pick games
+     */
+    BLINK_PICK = 460,
+    /**
+     * 3v3 Ranked Flex games
+     */
+    RANKED_FLEX = 470,
+    INTERMEDIATE_BOTS = 800,
+    INTRO_BOTS = 810,
+    BEGINNER_BOTS = 820,
+}
+
+export enum CrystalScar {
+    /**
+     * Definitely Not Dominion games
+     */
+    DEFINITELY_NOT_DOMINION = 317,
+    ASCENSION = 910
+}
+
+
+export enum ButchersBridge {
+    /**
+     * 5v5 ARAM games
+     */
+    ARAM = 100
+}
+
+export enum CosmicRuins {
+    /**
+     * Dark Star: Singularity games
+     */
+    DARK_STAR_SINGULARITY = 610
+}
+
+export enum ValoranCityPark {
+    /**
+     * Star Guardian Invasion: Normal games
+     */
+    INVASION_STANDARD = 980,
+    /**
+     * Star Guardian Invasion: Onslaught games
+     */
+    INVASION_ONSLAUGHT = 990
+}
+
+export enum Overcharge {
+    /**
+     * PROJECT: Hunters games
+     */
+    PROJECT_HUNTERS = 1000
+}
+
+export enum CrashSite {
+    ODYSSEY_INTRO = 1030,
+    ODYSSEY_CADET = 1040,
+    ODYSSEY_CREWMEMBER = 1050,
+    ODYSSEY_CAPTAIN = 1060,
+    ODYSSEY_ONSLAUGHT = 1070
+}
+
+export enum OtherQueues {
+    CUSTOM_GAMES = 0
+}
